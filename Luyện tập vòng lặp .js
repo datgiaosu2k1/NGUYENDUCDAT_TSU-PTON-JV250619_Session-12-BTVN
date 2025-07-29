0f73d6f640cf491c2b0239686e2aabec23e0548f
datgiaosu2k1 <datgiaosu2k1@gmail.com>
2025-07-29 20:28:20 +0900

    upload BTVN session 12

--- Luyện tập vòng lặp .js
+++ Luyện tập vòng lặp .js
@@ -82,21 +82,89 @@
 // }
 
 // bài 8
-let min = +prompt (" nhập số lớn nhất");
-let max = +prompt (" nhập số nhỏ nhất");
-let randum = Math.floor(Math.random() * (max - min + 1)) + min;
-let guess;
-whlie (true){
-    guess = +prompt("Nhập số bạn đoán (giữa " + min + " và " + max + "):");
-    if (guess < min || guess > max) {
-        alert("Số bạn nhập không hợp lệ. Vui lòng nhập lại.");
-    } else if (guess < randum) {
-        alert("Số bạn đoán quá nhỏ. Hãy thử lại.");
-    } else if (guess > randum) {
-        alert("Số bạn đoán quá lớn. Hãy thử lại.");
-    } else {
-        alert("Chúc mừng! Bạn đã đoán đúng số: " + randum);
-        break;
-    }
+// let min = +prompt (" nhập số nhỏ nhất");
+// let max = +prompt (" nhập số lớn nhất");
+// let randum = Math.floor(Math.random() * (max - min + 1)) + min;
+// let guess;
+// while (true){
+//     guess = +prompt("Nhập số bạn đoán (giữa " + min + " và " + max + "):");
+//     if (guess < min || guess > max) {
+//         alert("Số bạn nhập không hợp lệ. Vui lòng nhập lại.");
+//     } else if (guess < randum) {
+//         alert("Số bạn đoán quá nhỏ. Hãy thử lại.");
+//     } else if (guess > randum) {
+//         alert("Số bạn đoán quá lớn. Hãy thử lại.");
+//     } else {
+//         alert("Chúc mừng! Bạn đã đoán đúng số: " + randum);
+//         break;
+//     }
 
-}
+// }
+
+
+// -------------------- luyện tập 2 ---------------------------
+// bài 1
+// let a = 0;
+// let b = 1;
+// for ( let i = 1; i <= 20; i++) {
+//     console.log(a);
+//     let c = a + b;
+//     a = b;
+//     b = c;
+// }
+
+// bài 2
+// let n = +prompt("Nhập một số nguyên dương:");
+// let result = 1;
+// for (i = 1; i <= n; i = i + 1){
+//     result = result * i;
+// }
+// console.log("Giai thừa của " + n + " là: " + result);
+
+// bài 3
+// let h = 5;
+// for (let i = 1; i <= h; i++) {
+//   console.log("*".repeat(i));
+// }
+
+// for (let i = h; i >= 1; i--) {
+//   console.log("*".repeat(i));
+// }
+
+// for (let i = 1; i <= h; i++) {
+//   let space = " ".repeat(h - i);
+//   let star = "*".repeat(i);
+//   console.log(space + star);
+// }
+
+// for (let i = h; i >= 1; i--) {
+//   let space = " ".repeat(h - i);
+//   let star = "*".repeat(i);
+//   console.log(space + star);
+// }
+
+// bài 4
+// let rows = 5;
+// let cols = 10;
+
+// for (let i = 1; i <= rows; i++) {
+//   let line = "";
+//   for (let j = 1; j <= cols; j++) {
+//     if (i === 1 || i === rows || j === 1 || j === cols) {
+//       line += "*";
+//     } else {
+//       line += " ";
+//     }
+//   }
+//   console.log(line);
+// }
+
+// bài 5
+
+// let tienGoc = +prompt("Nhập số tiền gốc:");
+// let laiSuat = +prompt("Nhập lãi suất hàng năm ");
+// let soThang = +prompt("Nhập số tháng gửi:");
+// let tongTien = tienGoc * Math.pow((1 + laiSuat / 100 / 12), soThang);
+// let lai = tongTien - tienGoc;
+// console.log("Số tiền lãi sau " + soThang + " tháng là: " + lai.toFixed(2));
+// console.log("Tổng số tiền sau " + soThang + " tháng là: " + tongTien.toFixed(2));
